a25bdaf2a64111b2cda8eb5888c69f0530a73fa5
André Barros <andrebarros7201@gmail.com>
2025-05-31 21:03:41 +0100

    set diagnostics keymap and set Esc to clear highlighted text

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -26,3 +26,8 @@ vim.keymap.set("n", "<C-Right>", ":vertical +2<CR>")
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
+-- Open Diagnostics
+vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist)
+
+-- Clear highlighted words
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
