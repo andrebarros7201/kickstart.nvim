c18e0edd13c4f716912839f2dfdacb500fb1ef5c
André Barros <andrebarros7201@gmail.com>
2025-06-03 15:11:07 +0100

    set leader pv to open file explorer

--- lua/kickstart/plugins/oil.lua
+++ lua/kickstart/plugins/oil.lua
@@ -1,30 +1,25 @@
 return {
   'stevearc/oil.nvim',
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
   keys = {
-    { '<leader>pv', '<cmd>Oil<CR>', desc = 'Open File Explorer' },
+    { '-', '<cmd>Oil<CR>', desc = 'Open parent directory (Oil)' },
+    { '<leader>pv', '<cmd>Oil<CR>', desc = 'Open Oil file explorer' },
   },
-  config = function()
-    require('oil').setup {
-      default_file_explorer = true,
-      columns = { 'icon' },
-      keymaps = {
-        ['g?'] = { action = 'actions.show_help', mode = 'n' },
-        ['<CR>'] = 'actions.select',
-        ['<C-s>'] = { action = 'actions.select', opts = { vertical = true } },
-        ['<C-h>'] = { action = 'actions.select', opts = { horizontal = true } },
-        ['<C-t>'] = { action = 'actions.select', opts = { tab = true } },
-        ['<C-p>'] = 'actions.preview',
-        ['<C-c>'] = { action = 'actions.close', mode = 'n' },
-        ['<C-l>'] = 'actions.refresh',
-        ['-'] = { action = 'actions.parent', mode = 'n' },
-        ['_'] = { action = 'actions.open_cwd', mode = 'n' },
-        ['`'] = { action = 'actions.cd', mode = 'n' },
-        ['~'] = { action = 'actions.cd', opts = { scope = 'tab' }, mode = 'n' },
-        ['gs'] = { action = 'actions.change_sort', mode = 'n' },
-        ['gx'] = 'actions.open_external',
-        ['g.'] = { action = 'actions.toggle_hidden', mode = 'n' },
-        ['g\\'] = { action = 'actions.toggle_trash', mode = 'n' },
-      },
-    }
-  end,
+  opts = {
+    default_file_explorer = true,
+    columns = {
+      'icon',
+      'permissions',
+    },
+    view_options = {
+      show_hidden = false,
+    },
+    keymaps = {
+      ['q'] = 'actions.close',
+      ['<CR>'] = 'actions.select',
+      ['<C-s>'] = 'actions.select_vsplit',
+      ['<C-h>'] = 'actions.select_split',
+      ['<C-p>'] = 'actions.preview',
+    },
+  },
 }
