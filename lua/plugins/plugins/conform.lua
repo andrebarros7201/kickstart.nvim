a20dc8b0cf1da53111d8e5791a7cab155720b6d3
André Barros <andrebarros7201@gmail.com>
2025-06-03 17:27:20 +0100

    stable config

--- lua/plugins/plugins/conform.lua
+++ lua/plugins/plugins/conform.lua
@@ -31,11 +31,11 @@ return {
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
-        -- Conform can also run multiple formatters sequentially
-        -- python = { "isort", "black" },
-        --
-        -- You can use 'stop_after_first' to run the first available formatter from the list
-        -- javascript = { "prettierd", "prettier", stop_after_first = true },
+        javascript = { 'prettierd' },
+        typescript = { 'prettierd' },
+        json = { 'prettierd' },
+        html = { 'prettierd' },
+        css = { 'prettierd' },
       },
     },
   },
