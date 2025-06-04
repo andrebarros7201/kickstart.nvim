adf029cb80322b7137e0c65bece287d8e4d62738
André Barros <andrebarros7201@gmail.com>
2025-06-04 10:48:39 +0100

    add prettier formatter to jsx and tsx

--- lua/plugins/plugins/conform.lua
+++ lua/plugins/plugins/conform.lua
@@ -32,7 +32,9 @@ return {
       formatters_by_ft = {
         lua = { 'stylua' },
         javascript = { 'prettierd' },
+        javascriptreact = { 'prettierd' },
         typescript = { 'prettierd' },
+        typescriptreact = { 'prettierd' },
         json = { 'prettierd' },
         html = { 'prettierd' },
         css = { 'prettierd' },
