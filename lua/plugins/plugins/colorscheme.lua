0fab431650202ccc78a9455d007925ebffd3a861
André Barros <andrebarros7201@gmail.com>
2025-06-09 19:40:40 +0100

    change colorscheme

--- lua/plugins/plugins/colorscheme.lua
+++ lua/plugins/plugins/colorscheme.lua
@@ -1,12 +1,8 @@
 return {
-  {
-    'kepano/flexoki-neovim',
-    name = 'flexoki',
-    lazy = false,
-    priority = 1000,
-    config = function()
-      vim.cmd.colorscheme 'flexoki-dark'
-    end,
-  },
+  'Shatur/neovim-ayu',
+  config = function()
+    vim.cmd 'colorscheme ayu'
+    vim.o.background = 'dark' -- or "light" for the light variant
+  end,
 }
 -- vim: ts=2 sts=2 sw=2 et
