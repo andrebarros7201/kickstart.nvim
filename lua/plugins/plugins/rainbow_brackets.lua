b1c96f94fcb3578b691b21930e5e3425b86691a8
André Barros <andrebarros7201@gmail.com>
2025-06-12 13:50:53 +0100

    delete which key and restore rainbow brackets config

--- lua/plugins/plugins/rainbow_brackets.lua
+++ lua/plugins/plugins/rainbow_brackets.lua
@@ -1,16 +1,4 @@
 return {
-  'HiPhish/nvim-ts-rainbow2',
-  dependencies = 'nvim-treesitter/nvim-treesitter',
-  config = function()
-    require('nvim-treesitter.configs').setup {
-      highlight = {
-        enable = true,
-      },
-      rainbow = {
-        enable = true,
-        query = 'rainbow-parens',
-        strategy = require('ts-rainbow').strategy.global,
-      },
-    }
-  end,
+  'hiphish/rainbow-delimiters.nvim',
+  dependencies = { 'nvim-treesitter/nvim-treesitter' },
 }
