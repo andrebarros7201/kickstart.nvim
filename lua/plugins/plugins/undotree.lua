910a748a017bb9a351ab8b07f03d01efbf5b5a25
André Barros <andrebarros7201@gmail.com>
2025-06-13 21:34:15 +0100

    config undotree

--- lua/plugins/plugins/undotree.lua
+++ lua/plugins/plugins/undotree.lua
@@ -1,8 +1,13 @@
 return {
   'jiaoshijie/undotree',
   dependencies = 'nvim-lua/plenary.nvim',
-  config = true,
   keys = { -- load the plugin only when using it's keybinding:
     { '<leader>u', "<cmd>lua require('undotree').toggle()<cr>" },
   },
+  config = function()
+    require('undotree').setup {
+      float_diff = false,
+      position = 'right',
+    }
+  end,
 }
